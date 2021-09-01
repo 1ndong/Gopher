40a0cb971036c4cde0f6ae0a3b57f10f2eb1d3d3
1ndong <saint.laurent@kakao.com>
2021-09-01 22:11:22 +0900

    struct

--- struct.go
+++ struct.go
@@ -7,6 +7,32 @@ type Person struct {
 	age  int
 }
 
+type House struct {
+	Address  string
+	Size     int
+	Price    int
+	Category string
+}
+
+type User struct {
+	Name  string
+	ID    string
+	Age   int
+	Level int
+}
+
+type VIPUser struct {
+	User  // inheritance
+	Price int
+	Level int
+}
+
+type Test struct {
+	a int32
+	b float64
+	//a 4byte b 8 -> unsafe.Sizeof(Test) -> 12x , 16(because data padding)
+}
+
 func main() {
 	p := Person{}
 
@@ -18,5 +44,9 @@ func main() {
 	{
 		p := new(Person) // pointer
 		p.name = "nam"
+	}
+	{
+		var house House = House{"서울시 서초구", 18, 50, "빌라"}
+		fmt.Println(house)
 	}
 }
